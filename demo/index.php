a0c5283a4424430d6f085db9f38856da481f526b
Julio Jimenez <julio@julioj.com>
2024-01-29 20:21:37 -0500

    demo
    Signed-off-by: Julio Jimenez <julio@julioj.com>

---
+++ demo/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Demo</title>
+</head>
+
+<body>
+    <h1><?php echo "Hello World!"; ?></h1>
+</body>
+
+</html>
