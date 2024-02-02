950b4ca340d62fae4e962e2427369e2560159a40
Julio Jimenez <julio@julioj.com>
2024-02-01 19:53:55 -0500

    variables
    Signed-off-by: Julio Jimenez <julio@julioj.com>

---
+++ variables/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Demo</title>
+</head>
+
+<body>
+    <h1>
+        <?php
+        $greeting = "Hello";
+        echo $greeting . " " . "World!";
+        ?>
+    </h1>
+    <h2>
+        <?php
+        $name = "World";
+        echo "$greeting $name!";
+        ?>
+</body>
+
+</html>
